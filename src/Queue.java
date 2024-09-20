486ee164bd44a0758028dcebc5747802d4110edd
SovanMaoAUPP <2023476mao@aupp.edu.kh>
2024-09-20 11:00:03 +0700

    Done Q

--- src/Queue.java
+++ src/Queue.java
@@ -5,7 +5,7 @@ class Queue <T> {
     private int rear;
     private int nItems;
 
-    public Queue(int s) {         // constructor
+    public Queue(int s) {           // constructor
         maxSize = s;
         queArray = (T[]) new Object[maxSize];
         front = 0;
@@ -13,26 +13,26 @@ class Queue <T> {
         nItems = 0;
     }
 
-    public void insert(T j) {  // put item at rear of queue
-        if (rear == maxSize-1)    // deal with wraparound
+    public void insert(T j) {       // put item at rear of queue
+        if (rear == maxSize-1)      // deal with wraparound
             rear = -1;
-        queArray[++rear] = j;     // increment rear and insert
-        nItems++;                 // one more item
+        queArray[++rear] = j;       // increment rear and insert
+        nItems++;                   // one more item
     }
 
-    public T remove() {        // take item from front of queue
+    public T remove() {             // take item from front of queue
         T temp = queArray[front++]; // get value and incr front
-        if (front == maxSize)     // deal with wraparound
+        if (front == maxSize)       // deal with wraparound
             front = 0;
-        nItems--;                 // one less item
+        nItems--;                   // one less item
         return temp;
     }
 
-    public T peekFront() {     // peek at front of queue
+    public T peekFront() {          // peek at front of queue
         return queArray[front];
     }
 
-    public T peekRear() {     // peek at rear of queue
+    public T peekRear() {           // peek at rear of queue
         return queArray[rear];
     }
 
@@ -43,7 +43,7 @@ class Queue <T> {
         }
     }
 
-    // toString method
+    // toString method to return a string representation of the queue
     public String toString() {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < nItems; i++) {
