c4629f370ddc13d8f6459e34942564ef62b888a4
SovanMaoAUPP <2023476mao@aupp.edu.kh>
2024-09-20 12:26:43 +0700

    Done StringReverser

--- src/StringReverser.java
+++ src/StringReverser.java
@@ -1,7 +1,7 @@
 public class StringReverser {
     public static String reverse(String input) {
         // Stack to hold characters of the string
-        Stack<String> stack = new Stack<>();
+        Stack<String> stack = new Stack<>(input.length());
 
         // Push each character of the input string into the stack
         for (int i = 0; i < input.length(); i++) {
