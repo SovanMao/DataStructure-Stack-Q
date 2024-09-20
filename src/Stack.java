7e738df84281f398dee68b08aef5a643ce7b4242
SovanMaoAUPP <2023476mao@aupp.edu.kh>
2024-09-20 10:59:32 +0700

    Done Stack

--- src/Stack.java
+++ src/Stack.java
@@ -1,33 +1,34 @@
 class Stack<T> {
-    private int maxSize;        // size of stack array
+    private int maxSize;                // size of stack array
     private T[] stackArray;
-    private int top;            // top of stack
+    private int top;                    // top of stack
 
-    public Stack(int s) {              // constructor
+    public Stack(int s) {               // constructor
         maxSize = s;                    // set array size
         stackArray = (T[]) new Object[maxSize]; // create array
         top = -1;                       // no items yet
     }
 
-    public void push(T j) {          // put item on top of stack
+    public void push(T j) {             // put item on top of stack
         stackArray[++top] = j;          // increment top, insert item
     }
 
-    public T pop() {                 // take item from top of stack
+    public T pop() {                    // take item from top of stack
         return stackArray[top--];       // access item, decrement top
     }
 
-    public T peek() {                // peek at top of stack
+    public T peek() {                   // peek at top of stack
         return stackArray[top];
     }
 
+    // Method to display all elements in the stack from top to bottom
     public void display() {
         for (int i = top; i >= 0; i--) {
             System.out.println(stackArray[i]);
         }
     }
 
-    // toString method
+    // toString method to return a string representation of the stack
     public String toString() {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i <= top; i++) {
