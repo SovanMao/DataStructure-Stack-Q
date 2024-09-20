28da36a7ed972413cd1ea78caf646603d2890afe
SovanMaoAUPP <2023476mao@aupp.edu.kh>
2024-09-20 10:44:27 +0700

    Done Q

--- src/Queue.java
+++ src/Queue.java
@@ -1,46 +1,55 @@
-class Queue {
+class Queue <T> {
     private int maxSize;
-    private long[] queArray;
+    private T[] queArray;
     private int front;
     private int rear;
     private int nItems;
 
     public Queue(int s) {         // constructor
         maxSize = s;
-        queArray = new long[maxSize];
+        queArray = (T[]) new Object[maxSize];
         front = 0;
         rear = -1;
         nItems = 0;
     }
 
-    public void insert(long j) {  // put item at rear of queue
+    public void insert(T j) {  // put item at rear of queue
         if (rear == maxSize-1)    // deal with wraparound
             rear = -1;
         queArray[++rear] = j;     // increment rear and insert
         nItems++;                 // one more item
     }
 
-    public long remove() {        // take item from front of queue
-        long temp = queArray[front++]; // get value and incr front
+    public T remove() {        // take item from front of queue
+        T temp = queArray[front++]; // get value and incr front
         if (front == maxSize)     // deal with wraparound
             front = 0;
         nItems--;                 // one less item
         return temp;
     }
 
-    public long peekFront() {     // peek at front of queue
+    public T peekFront() {     // peek at front of queue
         return queArray[front];
     }
 
-    public boolean isEmpty() {   // true if queue is empty
-        return (nItems==0);
+    public T peekRear() {     // peek at rear of queue
+        return queArray[rear];
     }
 
-    public boolean isFull() {    // true if queue is full
-        return (nItems==maxSize);
+    // Display method
+    public void display() {
+        for (int i = 0; i < nItems; i++) {
+            System.out.println(queArray[(front + i) % queArray.length]);
+        }
     }
 
-    public int size() {          // number of items in queue
-        return nItems;
+    // toString method
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < nItems; i++) {
+            sb.append(queArray[(front + i) % queArray.length]).append(" ");
+        }
+        return sb.toString();
     }
+
 } // end class Queue
